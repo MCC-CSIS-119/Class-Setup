b7769dfc83b95e0caba49e643fdfa7e59da39109
Daniel Lloyd <dnlloyd@outlook.com>
2025-11-01 10:38:08 -0500

    final student login check

--- student_login_check.sh
+++ student_login_check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-awk -F: '/:x:10[0-9][0-9]/ && $1 !~ /dlloyd|ec2-user|bastion/ {print $1}' /etc/passwd | \
+awk -F: '/:x:10[0-9][0-9]/ && $1 !~ /ec2-user|bastion/ {print $1}' /etc/passwd | \
     while IFS= read -r student
     do
         if [[ -f "/home/${student}/.bash_history" ]]
@@ -8,5 +8,16 @@ awk -F: '/:x:10[0-9][0-9]/ && $1 !~ /dlloyd|ec2-user|bastion/ {print $1}' /etc/p
             echo "${student}: PASS"
         else
             echo "${student}: FAIL"
+        fi
+
+        if [[ ! -d "/home/${student}/my_scripts" ]]
+        then
+            mkdir "/home/${student}/my_scripts"
+        fi
+
+        if [[ ! -f "/home/${student}/my_scripts/hello.sh" ]]
+        then
+            touch "/home/${student}/my_scripts/hello.sh"
+            echo "echo \"Hello world!\"" > "/home/${student}/my_scripts/hello.sh"
         fi
     done
